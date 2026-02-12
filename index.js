6ef33f5cbb214ef7d694c87bbd8df340f1279637
Sharely Lilian Gonalez Velazquez <240639@utxicotepec.edu.mx>
2026-02-12 11:23:11 -0600

    PC-BienesRaices - Seccion 02
    add:Se agrego el archivo de rutas para los usuarios

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import usuarioRoutes from "./routes/usuarioRoutes.js";
+
+const app = express();
+const PORT = process.env.PORT ?? 4000;
+
+app.use(express.json());
+
+// GET principal
+app.get("/", (req, res) => {
+  console.log("Se está procesando la petición del tipo GET");
+
+  res.json({
+    status: 200,
+    message: "Bienvenido al Sistema de Bienes Raices",
+  });
+});
+
+//Conectar las rutas
+app.use("/", usuarioRoutes);
+
+app.listen(PORT, () => {
+  console.log(`El servidor está iniciado en el puerto ${PORT}`);
+});
